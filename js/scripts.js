ce1cd816452b85bd829a31ba2dd1c11667e9f94b
Cameron Anderson and Michael Albers <cameron@>
2018-01-08 10:52:53 -0800

    create buttons, and internal logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,63 @@
-var add = function(number1, number2) {
-  return number1 + number2;
-};
+// var add = function(number1, number2) {
+//   return number1 + number2;
+// };
+//
+// var subtract = function(number1, number2) {
+//   return number1 - number2;
+// };
+//
+// var multiply = function(number1, number2) {
+//   return number1 * number2;
+// };
+//
+// var divide = function(number1, number2) {
+//   return number1 / number2;
+// };
+//
+// var number1 = parseInt(prompt("Enter a number:"));
+// var number2 = parseInt(prompt("Enter another number:"));
+// var result = divide(number1, number2);
+// alert(result);
 
-var subtract = function(number1, number2) {
-  return number1 - number2;
-};
+var input = 0;
+var result = 0;
+var operation;
 
-var multiply = function(number1, number2) {
-  return number1 * number2;
-};
+$(".button").click(function () {
+  input = input * 10;
+  input = input + parseInt($(this).html());
+  // alert(input);
+ });
 
-var divide = function(number1, number2) {
-  return number1 / number2;
-};
+ $(".operator").click(function () {
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = divide(number1, number2);
-alert(result);
+   operation = $(this).html().trim();
+
+   if (operation === "+") {
+     result = result + input;
+   } else if (operation === "-") {
+     result = result - input;
+   } else if (operation === "/") {
+     result = result / input;
+   } else if (operation === "*") {
+     result = result * input;
+   }
+   alert(result);
+   input = 0;
+  //  alert(value1 + " " + operation);
+  });
+
+  $(".equals").click(function () {
+    if (operation === "+") {
+      alert(result + input);
+    } else if (operation === "-") {
+      alert(result - input);
+    } else if (operation === "/") {
+      alert(result / input);
+    } else if (operation === "*") {
+      alert(result * input);
+    }
+    input=0;
+    result=0;
+    operation="";
+   });
